52204ff26785a9d08fe1240e075046e0a3143957
Ran Nozik <ran@gethelios.dev>
2023-02-20 09:46:34 +0200

    remove condition

--- src/frontend/utils/telemetry/InstrumentationMiddleware.ts
+++ src/frontend/utils/telemetry/InstrumentationMiddleware.ts
@@ -7,42 +7,33 @@ const InstrumentationMiddleware = (handler: NextApiHandler, route?: string): Nex
     const { headers, method, url = '', httpVersion } = request;
     const [target] = url.split('?');
 
-    let span: Span;
-    const baggage = propagation.getBaggage(context.active());
-    if (baggage?.getEntry('synthetic_request')?.value == 'true') {
-      // if synthetic_request baggage is set, create a new trace linked to the span in context
-      // this span will look similar to the auto-instrumented HTTP span
-      const syntheticSpan = trace.getSpan(context.active()) as Span;
-      const tracer = trace.getTracer('@opentelemetry/instrumentation-http');
-      const attributes = {
-        'app.synthetic_request': true,
-        [SemanticAttributes.HTTP_TARGET]: target,
-        [SemanticAttributes.HTTP_STATUS_CODE]: response.statusCode,
-        [SemanticAttributes.HTTP_METHOD]: method,
-        [SemanticAttributes.HTTP_USER_AGENT]: headers['user-agent'] || '',
-        [SemanticAttributes.HTTP_URL]: `http://${headers.host}${url}`,
-        [SemanticAttributes.HTTP_FLAVOR]: httpVersion,
-        'http.request.headers': JSON.stringify(request.headers)
-      };
-
-      if (request.body) {
-        attributes['http.request.body'] = JSON.stringify(request.body)
-      }
-
-      if (route) {
-        attributes[SemanticAttributes.HTTP_ROUTE] = route;
-      }
-
-      span = tracer.startSpan(`HTTP ${method}`, {
-        root: true,
-        kind: SpanKind.SERVER,
-        links: [{ context: syntheticSpan.spanContext() }],
-        attributes,
-      });
-    } else {
-      // continue current trace/span
-      span = trace.getSpan(context.active()) as Span;
+    const syntheticSpan = trace.getSpan(context.active()) as Span;
+    const tracer = trace.getTracer('@opentelemetry/instrumentation-http');
+    const attributes = {
+      'app.synthetic_request': true,
+      [SemanticAttributes.HTTP_TARGET]: target,
+      [SemanticAttributes.HTTP_STATUS_CODE]: response.statusCode,
+      [SemanticAttributes.HTTP_METHOD]: method,
+      [SemanticAttributes.HTTP_USER_AGENT]: headers['user-agent'] || '',
+      [SemanticAttributes.HTTP_URL]: `http://${headers.host}${url}`,
+      [SemanticAttributes.HTTP_FLAVOR]: httpVersion,
+      'http.request.headers': JSON.stringify(request.headers)
+    };
+    
+    if (request.body) {
+      attributes['http.request.body'] = JSON.stringify(request.body)
     }
+
+    if (route) {
+      attributes[SemanticAttributes.HTTP_ROUTE] = route;
+    }
+
+    const span = tracer.startSpan(`HTTP ${method}`, {
+      root: true,
+      kind: SpanKind.SERVER,
+      links: [{ context: syntheticSpan.spanContext() }],
+      attributes,
+    });
 
     try {
       const origJson = response.json
